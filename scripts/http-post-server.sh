79f91aa77ff538d9f6d040c9224a1e3d8322d2b8
soheibthriber <sohlarbi@gmail.com>
2025-08-19 13:40:02 +0200

    remove old build and test scripts

--- scripts/http-post-server.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Under Debian: apt install ucspi-tcp
-set -eou pipefail
-
-readonly scriptDir=$(dirname $0)
-
-tcpserver -D 127.0.0.1 9000 $scriptDir/http-post-server-cgi.sh
--- scripts/version.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-readonly TAG=`git describe --tags --abbrev=0 2> /dev/null || echo "UNKNOWN"`
-readonly VERSION=`(git describe --tags --long 2> /dev/null || echo "UNKNOWN") | sed "s/^$TAG-//"`
-readonly BRANCH=`git rev-parse --abbrev-ref HEAD 2> /dev/null || echo "UNKNOWN"`
-
-echo "\"$TAG-$BRANCH-rev$VERSION\""
